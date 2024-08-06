243400e269570cefa737633d1f50d2bc5add03b8
DevenSojitra137 <sojitradeven137@gmail.com>
2024-08-06 18:48:37 +0530

    second-push

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import clasees from'./Card.module.css'
+import React from "react";
+import clasees from "./Card.module.css";
 
 const Card = (props) => {
   return (
-    <div className={`${clasees.card} ${props.className} ${props.className}`}>{props.children}</div>
-  )
-}
+    <div className={`${clasees.card} ${props.className} ${props.className}`}>
+      {props.children}
+    </div>
+  );
+};
 
-export default Card
+export default Card;
